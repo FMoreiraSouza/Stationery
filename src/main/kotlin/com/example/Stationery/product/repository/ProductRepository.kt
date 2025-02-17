60ac579081856207e396883b78e1c62f13391661
FMoreiraSouza <fmoreirasouza701@gmail.com>
2025-02-17 03:34:47 -0300

    Reajuste para persistência de dados

--- src/main/kotlin/com/example/Stationery/product/repository/ProductRepository.kt
+++ src/main/kotlin/com/example/Stationery/product/repository/ProductRepository.kt
@@ -1,7 +1,6 @@
 package com.example.Stationery.product.repository
 
 import com.example.Stationery.product.entity.ProductEntity
-import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
